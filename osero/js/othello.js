b0b6748d4ab46a987ef4440446f960deb3cb4923
Fujii Shigeto <194069@ocsjoho.onmicrosoft.com>
2022-03-16 23:04:46 +0900

    minor improvements.

--- osero/js/othello.js
+++ osero/js/othello.js
@@ -3,34 +3,33 @@ class Board {
         this.width = width;
         this.height = height;
 
+        this.boardElement = document.createElement('table');
+        this.boardElement.id = 'mainTable';
+
         this.state = new Array(height);
         for (let y = 0; y < height; y++) {
-            this.state[y] = new Array(width);
-            for (let x = 0; x < width; x++) {
-                this.state[y][x] = new Koma();
-            }
-        }
-    }
-
-    /** 盤面のhtml要素を作成する。
-     * Return : HTMLTableElement
-     */
-    createBoardElement() {
-        const table = document.createElement('table');
-        table.id = 'mainTable';
-
-        for (let y = 0; y < this.height; y++) {
             const row = document.createElement('tr');
-            for (let x = 0; x < this.width; x++) {
-                const cell = document.createElement('td');
-                cell.appendChild(this.state[y][x].imgElement);
+            this.state[y] = new Array(width);
 
+            for (let x = 0; x < width; x++) {
+                const cell = document.createElement('td');
+                const koma = new Koma();
+                koma.imgElement.addEventListener('click', () => {
+                    // 暫定処理
+                    if (koma.state == 0) {
+                        koma.put(1);
+                    } else {
+                        koma.flip();
+                    }
+                });
+
+                this.state[y][x] = koma;
+
+                cell.appendChild(koma.imgElement);
                 row.appendChild(cell);
             }
-
-            table.appendChild(row);
+            this.boardElement.appendChild(row);
         }
-        return table;
     }
 }
 
@@ -54,16 +53,6 @@ class Koma {
 
         this.imgElement = document.createElement('img');
         this.imgElement.src = KOMAIMG[this.state];
-
-        this.imgElement.addEventListener('click', () => {
-            // 暫定処理
-            if (this.state == 0) {
-                this.put(1);
-            } else {
-                this.flip();
-            }
-        });
-
     }
 
     flip() {
@@ -81,5 +70,5 @@ window.onload = () => {
     const playground = document.getElementById("playground");
 
     const board = new Board(8, 8);
-    playground.appendChild(board.createBoardElement());
+    playground.appendChild(board.boardElement);
 }
