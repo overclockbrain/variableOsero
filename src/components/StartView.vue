ace89940ab2d5b2bd53561b9c440a95017155ed2
宮本裕也 <81284337+Miyamoto-my@users.noreply.github.com>
2022-07-18 10:34:46 +0900

    Add files via upload

---
+++ src/components/StartView.vue
@@ -0,0 +1,40 @@
+<!-- 
+  StartVue.vue
+  created.by Y.Miyamoto on 2022-07-04
+
+  - 画面の縦・横を設定
+  - 決定後 => OthelloGame.vue
+ -->
+
+<template>
+  <section>
+    <h2>盤面の設定</h2>
+    <div class="center">
+      <RangeSlider label="縦" v-model="height" />
+      <RangeSlider label="横" v-model="width" />
+    </div>
+    <button @click="doConfirm">確定</button>
+  </section>
+</template>
+
+<script>
+import RangeSlider from "./parts/RangeSlider.vue";
+export default {
+  name: "StartView",
+  data() {
+    return {
+      width: 6,
+      height: 6,
+    };
+  },
+  methods: {
+    doConfirm() {
+      this.$emit("sizeSelect", this.width, this.height);
+    },
+  },
+  components: { RangeSlider },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/parts/RangeSlider.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ label }}:
+    <input
+      type="range"
+      min="4"
+      max="16"
+      step="2"
+      :value="modelValue"
+      @input="$emit('update:modelValue', Number($event.target.value))"
+    />
+    {{ modelValue }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RangeSlider",
+  props: {
+    label: String,
+    modelValue: Number,
+  },
+  data() {
+    return {
+      value: 6,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
